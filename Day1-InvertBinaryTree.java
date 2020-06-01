f0576cff85f3b03359ce86473730cf09c782c3b5
Ankit Ratnawat <ratnawat.ankit1@gmail.com>
2020-06-01 12:54:14 +0530

    01 June challenge solution

---
+++ Day1-InvertBinaryTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invertTreeUtil(root);
+        return root;
+    }
+    
+    private void invertTreeUtil(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTreeUtil(root.left);
+        invertTreeUtil(root.right);
+        
+        
+    }
+}
